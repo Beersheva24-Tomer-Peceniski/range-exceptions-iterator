9613fbbcbe98f29f276123dcdfd508e80e7c70f1
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-08-14 16:09:45 +0300

    HW

--- src/main/java/telran/range/BallBrokenFloor.java
+++ src/main/java/telran/range/BallBrokenFloor.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class BallBrokenFloor {
     private int nFloors;
     private int minBrokenFloor;
+    
     public BallBrokenFloor(int nFloors) {
         this.nFloors = nFloors;
         minBrokenFloor = new Random().nextInt(1, nFloors + 1);
